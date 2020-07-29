0a686a8f497e50303ed5e1767d0222913a6efec3
hantang-lxy <1027930265@qq.com>
2020-07-29 18:17:02 +0800

    update file
    Signed-off-by: hantang-lxy <1027930265@qq.com>

---
+++ src/main/java/cn/myjava/ssmDemo/Person.java
@@ -0,0 +1,12 @@
+package cn.myjava.ssmDemo;
+
+/**
+ * @author 李雪阳
+ * @version 1.0
+ * @date 2020/7/29  18:15
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("来吧，展示！");
+    }
+}
